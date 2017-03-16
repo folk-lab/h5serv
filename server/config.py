99bee6c44af7fa4c6d924124f39ad44f6807a6bc
Jason Brudvik <Jason.Brudvik@maxiv.lu.se>
2017-03-16 12:45:12 +0100

    cleaned up config file a bit

--- server/config.py
+++ server/config.py
@@ -16,41 +16,49 @@ cfg = {
     'port':   5000,
     'debug':  True,
     'datapath': '../data/',
-    'public_dir': ['public', 'test'],
-    # 'domain':  'hdfgroup.org',
+    'public_dir': ['example-subfolder'],
     'domain':  'maxiv.lu.se',
     'hdf5_ext': '.h5',
     'toc_name': '.toc.h5',
     'home_dir': 'home',
     'ssl_port': 6050,
-    'ssl_cert': '',  # certs/data.hdfgroup.org.crt',  # add relative path to cert for SSL
-    'ssl_key':  '',  # certs/data.hdfgroup.org.key',  # add relative path to cert key for SSL
+    'ssl_cert': '',  # add relative path to cert for SSL
+    'ssl_key':  '',  # add relative path to cert key for SSL
     'ssl_cert_pwd': '',
     'password_uri': '../util/admin/passwd.h5',
-    #'password_uri': 'mongodb://mongo:27017',
+    # 'password_uri': 'mongodb://mongo:27017',
     'mongo_dbname': 'hdfdevtest',
     'static_url': r'/views/(.*)',
     'static_path': r'../static',
-    'cors_domain': '*',  # set to None to disallow CORS (cross-origin resource sharing)
+
+    # set to None to disallow CORS (cross-origin resource sharing)
+    'cors_domain': '*',
     'log_file': r'../log/h5serv.log',
-    # 'log_level': 'INFO', # ERROR, WARNING, INFO, DEBUG, or NOTSET,
-    'log_level': 'DEBUG', # ERROR, WARNING, INFO, DEBUG, or NOTSET,
-    'background_timeout': 1000  # (ms) set to 0 to disable background processing
+    'log_level': 'DEBUG',  # ERROR, WARNING, INFO, DEBUG, or NOTSET,
+
+    # (ms) set to 0 to disable background processing
+    'background_timeout': 1000
 }
 
 
 def get(x):
+
     # see if there is a command-line override
     option = '--'+x+'='
+
     for i in range(1, len(sys.argv)):
-        #print i, sys.argv[i]
+
+        # print i, sys.argv[i]
         if sys.argv[i].startswith(option):
+
             # found an override
             arg = sys.argv[i]
             return arg[len(option):]  # return text after option string
+
     # see if there are an environment variable override
     if x.upper() in os.environ:
         return os.environ[x.upper()]
+
     # no command line override, just return the cfg value
     if x in cfg:
         return cfg[x]
